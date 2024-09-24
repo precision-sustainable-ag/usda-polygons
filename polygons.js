ae4b5b1f8b67f15718234cd369a632e303797754
Rick Hitchcock <rickhitchcock@outlook.com>
2024-09-24 09:23:06 -0400

    county route

--- polygons.js
+++ polygons.js
@@ -1,29 +1,19 @@
 const { pool } = require('./pools');
 
-const routeMLRA = async (req, res) => {
+const query = async (req, res, query) => {
   const lat = +req.query.lat || +req.body.lat;
   const lon = +req.query.lon || +req.body.lon;
+  const point = `POINT(${lon} ${lat})`;
   const polygon = (req.query.polygon || req.body.polygon) === 'true';
+
+  query = query.replace('POLYGON', polygon ? ', ST_AsText(geometry) as polygon' : '');
 
   if (isNaN(lat) || isNaN(lon)) {
     return res.status(400).send({ error: 'Invalid or missing latitude/longitude' });
   }
 
-  const point = `POINT(${lon} ${lat})`;
-
-  const sq = `
-    SELECT
-      mlrarsym,
-      name,
-      lrrsym,
-      lrrname
-      ${polygon ? ', ST_AsText(geometry) as polygon' : ''}
-    FROM polygons.mlra
-    WHERE ST_Contains(geometry, ST_GeomFromText($1))
-  `;
-
   try {
-    const results = await pool.query(sq, [point]);
+    const results = await pool.query(query, [point]);
 
     if (results.rows.length) {
       res.send(results.rows[0]);
@@ -34,8 +24,43 @@ const routeMLRA = async (req, res) => {
     res.status(500).send({ error: 'Database error', details: err.message });
     console.error(err);
   }
+}; // query
+
+const routeCounty = (req, res) => {
+  query(req, res, `
+    SELECT
+      statefips,
+      countyfips,
+      countyns,
+      affgeoid,
+      geoid,
+      county,
+      namelsad,
+      state_code,
+      state,
+      lsad,
+      aland,
+      awater
+      POLYGON
+    FROM polygons.counties
+    WHERE ST_Contains(geometry, ST_SetSRID(ST_GeomFromText($1), 4269))
+  `);
+}; // routeCounty
+
+const routeMLRA = (req, res) => {
+  query(req, res, `
+    SELECT
+      mlrarsym,
+      name,
+      lrrsym,
+      lrrname
+      POLYGON
+    FROM polygons.mlra
+    WHERE ST_Contains(geometry, ST_SetSRID(ST_GeomFromText($1), 4269))
+  `);
 }; // routeMLRA
 
 module.exports = {
   routeMLRA,
+  routeCounty,
 };
