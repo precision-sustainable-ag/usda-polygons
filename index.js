98648d92bfdc3b7326215603b5e833ddb7336207
Rick Hitchcock <rickhitchcock@outlook.com>
2025-04-18 15:24:36 -0400

    more USDA troubleshooting shenanigans

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ process.on('uncaughtException', (err) => {
 
 app.use(cors());              // handles all origins
 app.options('*', cors());     // handles preflight, possible fix for USDA???
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
